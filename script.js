a2940a48654b4144a8c306bd07ff781c65425212
Ananas <mrs.anna.noga@gmail.com>
2023-01-08 01:46:34 +0000

    prompt 'how many?' works, password options works

--- script.js
+++ script.js
@@ -87,24 +87,73 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+//user input variables
+
+var passwordLength;
+var confirmSpecialCharacter; //= confirm("Would you like to include special character? Click OK to confirm.");
+var confirmNumericCharacters; //= confirm("Would you like to include number? Click OK to confirm.");
+var confirmLowerCasedCharacters; // = confirm("Would you like to include lowercased letter? Click OK to confirm.");
+var confirmUpperCasedCharacters; //= confirm("Would you like to include uppercased letter? Click OK to confirm.");
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+  confirmSpecialCharacter = confirm("Would you like to include special charackters? Click OK if yes.");
+  confirmNumericCharacters = confirm("Would you like to include numbers? Click OK if yes.");
+  confirmLowerCasedCharacters = confirm("Would you like to include lowercased letter? Click OK if yes.");
+  confirmUpperCasedCharacters = confirm("Would you like to include uppercased letter? Click OK if yes.");
+
+  if (confirmSpecialCharacter === false && confirmNumericCharacters === false && confirmLowerCasedCharacters === false && confirmUpperCasedCharacters === false) {
+    alert("You must choose at least one criteria! Please, try again.");
+    confirmSpecialCharacter = confirm("Would you like to include special charackters? Click OK if yes.");
+    confirmNumericCharacters = confirm("Would you like to include numbers? Click OK if yes.");
+    confirmLowerCasedCharacters = confirm("Would you like to include lowercased letter? Click OK if yes.");
+    confirmUpperCasedCharacters = confirm("Would you like to include uppercased letter? Click OK if yes.");
+  }
+
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom() {
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  passwordLength = prompt("How many characters would you like your password to contain?");
 
+  if (!passwordLength) {
+    passwordLength = prompt("You haven't chosen any number. Please, try again!");
+    if (passwordLength <= 9 || passwordLength >= 65) {
+      passwordLength = prompt("You have to choose between 10 and 64. Please, try again.");
+    }
+  } else if (passwordLength <= 9 || passwordLength >= 65) {
+    passwordLength = prompt("You have to choose between 10 and 64. Please, try again.");
+  }
+  alert("Your password will have " + passwordLength + " characters");
+
+  //call the password option function 
+  getPasswordOptions();
+  //code below makes lags
+  // if (!getPasswordOptions()) {
+  //   alert("You must choose at least one criteria! Please, try again.");
+  //   confirmSpecialCharacter = confirm("Would you like to include special charackters? Click OK if yes.");
+  //   confirmNumericCharacters = confirm("Would you like to include numbers? Click OK if yes.");
+  //   confirmLowerCasedCharacters = confirm("Would you like to include lowercased letter? Click OK if yes.");
+  //   confirmUpperCasedCharacters = confirm("Would you like to include uppercased letter? Click OK if yes.");
+  // }
 }
 
+//call the generate password function
+//generatePassword();
+// no need to change below code!!
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+//var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
@@ -115,4 +164,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+//generateBtn.addEventListener('click', writePassword);
